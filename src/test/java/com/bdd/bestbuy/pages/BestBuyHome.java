7964bc51cb81f8646274867fefbf211eb88c3f5e
Kumar <942296@cognizant.com>
2021-08-09 14:40:51 -0700

    first run thru on second part

--- src/test/java/com/bdd/bestbuy/pages/BestBuyHome.java
+++ src/test/java/com/bdd/bestbuy/pages/BestBuyHome.java
@@ -13,6 +13,26 @@ public class BestBuyHome {
         this.driver.get(url);
     }
 
+    public WebElement getSearchResultsLabel() {
+        return driver.findElement(By.className("search-title"));
+    }
+
+    public WebElement getAddToCartButton() {
+        return driver.findElement(By.xpath("//*[@id='fulfillment-add-to-cart-button-96125401']/div/div/div/button"));
+    }
+
+    public void switchToCartSubtotalModal() {
+        driver.switchTo().activeElement();
+    }
+
+    public WebElement getCartSubtotalLabel() {
+        return driver.findElement(By.xpath("//*[@id='shop-cart-subtotal-61265006']/div/div/div[1]"));
+    }
+
+    public WebElement getCartSubtotalPrice() {
+        return driver.findElement(By.xpath("//*[@id='shop-cart-subtotal-61265006']/div/div/div[2]"));
+    }
+
     public void Quit() {
         driver.quit();
     }
--- src/test/java/com/bdd/bestbuy/stepdefs/BestBuyStepdefs.java
+++ src/test/java/com/bdd/bestbuy/stepdefs/BestBuyStepdefs.java
@@ -1,10 +1,14 @@
 package com.bdd.bestbuy.stepdefs;
 
+import com.bdd.bestbuy.pages.BestBuyHome;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class BestBuyStepdefs {
+
+    private BestBuyHome bestBuyHome;
 
     @Given("I am on the Best Buy home page")
     public void iAmOnTheBestBuyHomePage() {
@@ -23,17 +27,25 @@ public class BestBuyStepdefs {
 
     @Given("I perform the above search")
     public void iPerformTheAboveSearch() {
-        
+        Assert.assertEquals("\"macbook pro\"", bestBuyHome.getSearchResultsLabel().getText());
     }
 
     @When("I click the 'Add to Cart' button next to the laptop")
     public void iClickTheAddToCartButtonNextToTheLaptop() {
-        
+        bestBuyHome.getAddToCartButton().click();
     }
 
     @Then("I should see a modal window with the cart subtotal")
     public void iShouldSeeAModalWindowWithTheCartSubtotal() {
-        
+        // Switch to modal
+        bestBuyHome.switchToCartSubtotalModal();
+
+        // Check modal for cart subtotal label
+        String cartSubtotalLabel = bestBuyHome.getCartSubtotalLabel().getText();
+        Assert.assertTrue(cartSubtotalLabel.contains("Subtotal"));
+
+        // Check cart subtotal price in modal
+        Assert.assertEquals("$1,099.99", bestBuyHome.getCartSubtotalPrice());
     }
 
     @Given("I am on the above modal page")
