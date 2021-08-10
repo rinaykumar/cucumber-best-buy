344ff3760e5e2149c45e540748e342a112df2775
Kumar <942296@cognizant.com>
2021-08-09 19:10:46 -0700

    test implementation

--- src/test/java/com/bdd/bestbuy/pages/BestBuyHome.java
+++ src/test/java/com/bdd/bestbuy/pages/BestBuyHome.java
@@ -3,6 +3,7 @@ package com.bdd.bestbuy.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import java.util.concurrent.TimeUnit;
 
 public class BestBuyHome {
     private String url = "https://www.bestbuy.com/";
@@ -13,12 +14,28 @@ public class BestBuyHome {
         this.driver.get(url);
     }
 
-    public WebElement getSearchResultsLabel() {
-        return driver.findElement(By.className("search-title"));
+    public void waitForPageLoad(int sec) {
+        driver.manage().timeouts().implicitlyWait(sec, TimeUnit.SECONDS);
+    }
+
+    public WebElement getAddModalCloseButton() {
+        return driver.findElement(By.xpath("//*[@id=\"widgets-view-email-modal-mount\"]/div/div/div[1]/div/div/div/div/button"));
+    }
+
+    public WebElement getSearchBar() {
+        return driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/header/div[1]/div/div[1]/div/form/input[1]"));
+    }
+
+    public WebElement getSearchButton() {
+        return driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/header/div[1]/div/div[1]/div/form/button[2]"));
+    }
+
+    public WebElement getMacBookInfo() {
+        return driver.findElement(By.xpath("/html/body/div[4]/main/div[10]/div/div/div/div/div/div/div[2]/div[2]/div[4]/div/div[6]/ol/li[2]/div/div/div/div/div/div[2]/div[1]/div[2]/div/h4/a"));
     }
 
     public WebElement getAddToCartButton() {
-        return driver.findElement(By.xpath("//*[@id='fulfillment-add-to-cart-button-96125401']/div/div/div/button"));
+        return driver.findElement(By.xpath("/html/body/div[4]/main/div[10]/div/div/div/div/div/div/div[2]/div[2]/div[4]/div/div[6]/ol/li[2]/div/div/div/div/div/div[2]/div[2]/div[3]/div/div/div/div/div/div/button"));
     }
 
     public void switchToCartSubtotalModal() {
@@ -26,11 +43,27 @@ public class BestBuyHome {
     }
 
     public WebElement getCartSubtotalLabel() {
-        return driver.findElement(By.xpath("//*[@id='shop-cart-subtotal-61265006']/div/div/div[1]"));
+        return driver.findElement(By.xpath("/html/body/div[9]/div/div[1]/div/div/div/div/div[1]/div[2]/div/div/div/div/div[1]"));
     }
 
-    public WebElement getCartSubtotalPrice() {
-        return driver.findElement(By.xpath("//*[@id='shop-cart-subtotal-61265006']/div/div/div[2]"));
+    public WebElement getGoToCartButton() {
+        return driver.findElement((By.xpath("/html/body/div[9]/div/div[1]/div/div/div/div/div[1]/div[3]/a")));
+    }
+
+    public WebElement getOrderSummaryLabel() {
+        return driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/h2"));
+    }
+
+    public WebElement getTotalLabel() {
+        return driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/div[1]/div/div[6]/div[1]/span"));
+    }
+
+    public WebElement getRemoveLink() {
+        return driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/div[1]/div/div[2]/div[1]/section[1]/div[4]/ul/li/section/div[2]/div[3]/a[1]"));
+    }
+
+    public WebElement getCartIsEmptyLabel() {
+        return driver.findElement(By.xpath("/html/body/div[1]/main/div/div[2]/div[1]/div/div[1]/div[1]/section[1]/div[1]/div/h1"));
     }
 
     public void Quit() {
--- src/test/java/com/bdd/bestbuy/stepdefs/BestBuyStepdefs.java
+++ src/test/java/com/bdd/bestbuy/stepdefs/BestBuyStepdefs.java
@@ -4,7 +4,10 @@ import com.bdd.bestbuy.pages.BestBuyHome;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BestBuyStepdefs {
 
@@ -12,69 +15,66 @@ public class BestBuyStepdefs {
 
     @Given("I am on the Best Buy home page")
     public void iAmOnTheBestBuyHomePage() {
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        bestBuyHome = new BestBuyHome(driver);
     }
 
     @When("I close the add modal and search for 'macbook pro'")
     public void iCloseTheAddModalAndSearchForMacbookPro() {
-        
+        bestBuyHome.waitForPageLoad(10);
+        bestBuyHome.getAddModalCloseButton().click();
+        bestBuyHome.getSearchBar().click();
+        bestBuyHome.getSearchBar().sendKeys("macbook pro");
+        bestBuyHome.getSearchButton().click();
     }
 
     @Then("One of the laptops listed should be 13.3 8GB Memory and 256GB SSD")
     public void oneOfTheLaptopsListedShouldBeGBMemoryAndGBSSD() {
-
-    }
-
-    @Given("I perform the above search")
-    public void iPerformTheAboveSearch() {
-        Assert.assertEquals("\"macbook pro\"", bestBuyHome.getSearchResultsLabel().getText());
+        Assert.assertEquals("MacBook Pro 13.3\" Laptop - Apple M1 chip - 8GB Memory - 256GB SSD (Latest Model) - Space Gray", bestBuyHome.getMacBookInfo().getText());
+        bestBuyHome.Quit();
     }
 
     @When("I click the 'Add to Cart' button next to the laptop")
     public void iClickTheAddToCartButtonNextToTheLaptop() {
+        bestBuyHome.waitForPageLoad(15);
         bestBuyHome.getAddToCartButton().click();
     }
 
     @Then("I should see a modal window with the cart subtotal")
     public void iShouldSeeAModalWindowWithTheCartSubtotal() {
-        // Switch to modal
+        // Switch to modal and wait
         bestBuyHome.switchToCartSubtotalModal();
+        bestBuyHome.waitForPageLoad(15);
 
         // Check modal for cart subtotal label
         String cartSubtotalLabel = bestBuyHome.getCartSubtotalLabel().getText();
         Assert.assertTrue(cartSubtotalLabel.contains("Subtotal"));
 
-        // Check cart subtotal price in modal
-        Assert.assertEquals("$1,099.99", bestBuyHome.getCartSubtotalPrice());
-    }
-
-    @Given("I am on the above modal page")
-    public void iAmOnTheAboveModalPage() {
-        
+        bestBuyHome.Quit();
     }
 
     @When("I click on the 'Go to Cart' button")
     public void iClickOnTheGoToCartButton() {
-        
+        bestBuyHome.getGoToCartButton().click();
+        bestBuyHome.waitForPageLoad(5);
     }
 
     @Then("I should see the laptop and the order summary with Total price")
     public void iShouldSeeTheLaptopAndTheOrderSummaryWithTotalPrice() {
-        
-    }
-
-    @Given("I am on the above page")
-    public void iAmOnTheAbovePage() {
-        
+        Assert.assertEquals("Order Summary", bestBuyHome.getOrderSummaryLabel().getText());
+        Assert.assertEquals("Total", bestBuyHome.getTotalLabel().getText());
+        bestBuyHome.Quit();
     }
 
     @When("I click the 'Remove' link under the item number drop down")
     public void iClickTheRemoveLinkUnderTheItemNumberDropDown() {
-        
+        bestBuyHome.getRemoveLink().click();
     }
 
     @Then("I my anonymous cart should be empty")
     public void iMyAnonymousCartShouldBeEmpty() {
-
+        Assert.assertEquals("Your cart is empty", bestBuyHome.getCartIsEmptyLabel().getText());
+        bestBuyHome.Quit();
     }
 }
