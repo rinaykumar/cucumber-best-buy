31dd9c044040ef0c651f4d32d16d4029650f5fa2
pf4cvsmania <63117077+pf4cvsmania@users.noreply.github.com>
2021-08-10 15:06:09 -0400

    Completed given, when and then

--- src/test/java/com/bdd/bestbuy/pages/BestBuyHome.java
+++ src/test/java/com/bdd/bestbuy/pages/BestBuyHome.java
@@ -1,16 +1,41 @@
 package com.bdd.bestbuy.pages;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.concurrent.TimeUnit;
+
 public class BestBuyHome {
+
     private String url = "https://www.bestbuy.com/";
     WebDriver driver;
 
-    public BestBuyHome(WebDriver driver){
+    public BestBuyHome(WebDriver driver) {
         this.driver = driver;
         this.driver.get(url);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public WebElement getSearchBar(){
+        return driver.findElement(By.id("gh-search-input"));
+    }
+
+    public WebElement submitSearch(){
+        return driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/header/div[1]/div/div[1]/div/form/button[2]"));
+    }
+
+    public void closeModal(){
+        driver.findElement(By.xpath("/html/body/main/div[2]/div[5]/div/div/div[1]/div/div/div/div/button")).click();
+    }
+
+    public String getMacbookModel(){
+        return driver.findElement(By.xpath("/html/body/div[4]/main/div[10]/div/div/div/div/div/div/div[2]/div[2]/div[4]/div/div[6]/ol/li[2]/div/div/div/div/div/div[2]/div[1]/div[2]/div/h4/a")).getText();
+    }
+
+    public void closeAccountModal(){
+        driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/header/div[2]/nav/ul/li[1]/div/div/button/svg")).click();
     }
 
     public void Quit() {
--- src/test/java/com/bdd/bestbuy/stepdefs/BestBuyStepdefs.java
+++ src/test/java/com/bdd/bestbuy/stepdefs/BestBuyStepdefs.java
@@ -4,6 +4,12 @@ import com.bdd.bestbuy.pages.BestBuyHome;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BestBuyStepdefs {
 
@@ -11,17 +17,25 @@ public class BestBuyStepdefs {
 
     @Given("I am on the Best Buy home page")
     public void iAmOnTheBestBuyHomePage() {
-
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        bestBuyHome = new BestBuyHome(driver);
     }
 
     @When("I close the add modal and search for 'macbook pro'")
-    public void iCloseTheAddModalAndSearchForMacbookPro() {
-        
+    public void iCloseTheAddModalAndSearchForMacbookPro() throws InterruptedException {
+        bestBuyHome.closeModal();
+        Thread.sleep(1000);
+        bestBuyHome.getSearchBar().sendKeys("macbook pro");
+        bestBuyHome.submitSearch().click();
+        Thread.sleep(10000);
     }
 
     @Then("One of the laptops listed should be 13.3 8GB Memory and 256GB SSD")
     public void oneOfTheLaptopsListedShouldBeGBMemoryAndGBSSD() {
-
+        String macBookDescription = bestBuyHome.getMacbookModel();
+        Assert.assertEquals("MacBook Pro 13.3\" Laptop - Apple M1 chip - 8GB Memory - 256GB SSD (Latest Model) - Space Gray" , macBookDescription );
+        bestBuyHome.Quit();
     }
 
     @Given("I perform the above search")
@@ -31,7 +45,7 @@ public class BestBuyStepdefs {
 
     @When("I click the 'Add to Cart' button next to the laptop")
     public void iClickTheAddToCartButtonNextToTheLaptop() {
-
+        
     }
 
     @Then("I should see a modal window with the cart subtotal")
